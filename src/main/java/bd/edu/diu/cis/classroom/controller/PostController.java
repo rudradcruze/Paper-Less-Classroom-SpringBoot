e262726b8e3db4c97b97715b9f1a7ffc1ed9f51c
Francis Rudra D Cruze <rudradcruze@Franciss-Mac-mini.local>
2023-09-09 08:09:01 +0600

    clean extra import

--- src/main/java/bd/edu/diu/cis/classroom/controller/PostController.java
+++ src/main/java/bd/edu/diu/cis/classroom/controller/PostController.java
@@ -10,7 +10,6 @@ import bd.edu.diu.cis.classroom.utils.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
